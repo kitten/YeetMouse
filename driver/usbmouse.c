6993bf086b0e9f9c5826436d7ba4b1cd375332cc
Phil Pluckthun <phil@kitten.sh>
2024-10-09 12:08:47 +0100

    temp: test inject_event

--- driver/usbmouse.c
+++ driver/usbmouse.c
@@ -23,94 +23,53 @@
 #define DRIVER_DESC "USB HID mouse driver with acceleration (LEETMOUSE)"
 
 #define NONE_EVENT_VALUE 0
-static int relative_axis_events[REL_CNT] = {
+static int usb_mouse_event_lock = 0;
+static int rel_event_values[REL_CNT] = {
   /* [x, y, ..., wheel, ...] */ NONE_EVENT_VALUE
 };
 
-struct input_dev *virtual_input_dev;
-
-// The virtual_input_dev is declared for/in the maccel_input_handler module.
-// This initializes it.
-static int create_virtual_device(void) {
-  int error;
-
-  virtual_input_dev = input_allocate_device();
-  if (!virtual_input_dev) {
-    printk(KERN_ERR "Failed to allocate virtual input device\n");
-    return -ENOMEM;
-  }
-
-  virtual_input_dev->name = "leetmouse";
-  virtual_input_dev->id.bustype = BUS_USB;
-  /* virtual_input_dev->id.vendor = 0x1234; */
-  /* virtual_input_dev->id.product = 0x5678; */
-  virtual_input_dev->id.version = 1;
-
-  // Set the supported event types and codes for the virtual device
-  // and for some reason not setting some EV_KEY bits causes a noticeable
-  // difference in the values we operate on, leading to a different
-  // acceleration behavior than we expect.
-  set_bit(EV_KEY, virtual_input_dev->evbit);
-  set_bit(BTN_LEFT, virtual_input_dev->keybit);
-
-  set_bit(EV_REL, virtual_input_dev->evbit);
-  for (u32 code = REL_X; code < REL_CNT; code++) {
-    set_bit(code, virtual_input_dev->relbit);
-  }
-
-  error = input_register_device(virtual_input_dev);
-  if (error) {
-    printk(KERN_ERR "LEETMOUSE: Failed to register virtual input device\n");
-    input_free_device(virtual_input_dev);
-    return error;
-  }
-
-  return 0;
+static void usb_mouse_report_rel(struct input_dev *dev, u32 code, int value) {
+  usb_mouse_event_lock++;
+  input_report_rel(dev, code, value);
 }
 
 static bool usb_mouse_filter(struct input_handle *handle, u32 type, u32 code, int value) {
   switch (type) {
-    case EV_REL: {
-      printk("LEETMOUSE: EV_REL => code %d, value %d", code, value);
-      relative_axis_events[code] = value;
-      return true; // skip unaccelerated mouse input.
-    }
     case EV_SYN: {
-      int x = relative_axis_events[REL_X];
-      int y = relative_axis_events[REL_Y];
-      int wheel = relative_axis_events[REL_WHEEL];
-      if (x || y || wheel) {
-        printk("LEETMOUSE: EV_SYN => code %d", code);
+      int x = rel_event_values[REL_X];
+      int y = rel_event_values[REL_Y];
+      int wheel = rel_event_values[REL_WHEEL];
 
-        if (!accelerate(&x, &y, &wheel)) {
-          if (x) {
-            input_report_rel(virtual_input_dev, REL_X, x);
-          }
-          if (y) {
-            input_report_rel(virtual_input_dev, REL_Y, y);
-          }
-          if (wheel) {
-            input_report_rel(virtual_input_dev, REL_WHEEL, wheel);
-          }
-        }
-
-        relative_axis_events[REL_X] = NONE_EVENT_VALUE;
-        relative_axis_events[REL_Y] = NONE_EVENT_VALUE;
-        relative_axis_events[REL_WHEEL] = NONE_EVENT_VALUE;
+      if (!accelerate(&x, &y, &wheel)) {
+        usb_mouse_report_rel(handle->dev, REL_X, x);
+        usb_mouse_report_rel(handle->dev, REL_Y, y);
+        usb_mouse_report_rel(handle->dev, REL_WHEEL, wheel);
       }
+
+      rel_event_values[REL_X] = NONE_EVENT_VALUE;
+      rel_event_values[REL_Y] = NONE_EVENT_VALUE;
+      rel_event_values[REL_WHEEL] = NONE_EVENT_VALUE;
 
       for (u32 code = REL_Z; code < REL_CNT; code++) {
-        int value = relative_axis_events[code];
-        if (value != NONE_EVENT_VALUE) {
-          input_report_rel(virtual_input_dev, code, value);
-          relative_axis_events[code] = NONE_EVENT_VALUE;
+        if (rel_event_values[code] != NONE_EVENT_VALUE) {
+          usb_mouse_report_rel(handle->dev, code, rel_event_values[code]);
+          rel_event_values[code] = NONE_EVENT_VALUE;
         }
       }
-
-      input_sync(virtual_input_dev);
 
       return false;
     }
+
+    case EV_REL:
+      if (code < REL_Z || code > REL_CNT) {
+        return false;
+      } else if (usb_mouse_event_lock > 0) {
+        usb_mouse_event_lock--;
+        return false;
+      } else {
+        rel_event_values[code] = value;
+        return true;
+      }
 
     default:
       return false;
@@ -118,7 +77,7 @@ static bool usb_mouse_filter(struct input_handle *handle, u32 type, u32 code, in
 }
 
 static bool usb_mouse_match(struct input_handler *handler, struct input_dev *dev) {
-  if (dev == virtual_input_dev || !dev->dev.parent)
+  if (!dev->dev.parent)
     return false;
   struct hid_device *hdev = to_hid_device(dev->dev.parent);
   printk("LEETMOUSE: found a possible mouse %s", hdev->name);
@@ -176,24 +135,18 @@ MODULE_DEVICE_TABLE(input, usb_mouse_ids);
 struct input_handler usb_mouse_handler = {
   .name = "leetmouse",
   .id_table = usb_mouse_ids,
-  .filter = usb_mouse_filter,
   .connect = usb_mouse_connect,
   .disconnect = usb_mouse_disconnect,
-  .match = usb_mouse_match
+  .match = usb_mouse_match,
+  .filter = usb_mouse_filter,
 };
 
 static int __init usb_mouse_init(void) {
-  int error = create_virtual_device();
-  if (error) {
-    return error;
-  }
   return input_register_handler(&usb_mouse_handler);
 }
 
 static void __exit usb_mouse_exit(void) {
   input_unregister_handler(&usb_mouse_handler);
-  input_unregister_device(virtual_input_dev);
-  input_free_device(virtual_input_dev);
 }
 
 MODULE_AUTHOR(DRIVER_AUTHOR);
