23f10e48998f9f2313e2ce06ca4f7e4872d2487c
Phil Pluckthun <phil@kitten.sh>
2025-02-21 18:32:42 +0000

    Add modprobe line to install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 # Install the driver and activate the dkms module
 sudo make setup_dkms
 sudo dkms install -m leetmouse-driver -v 0.9.0 # Enter the version you determined from the Makefile earlier in here
+sudo modprobe leetmouse
